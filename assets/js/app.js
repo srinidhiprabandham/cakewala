96e80ba732c8de49bafe63aade1588531768a327
Srinidhi Prabandham <srinidhi.prabandham@icicletech.com>
2018-03-23 11:37:34 +0530

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,259 @@
+window.objects_json = {
+    "regular_cakes": ["ALMONDALPHONSO.jpg", "CHOCOTEMPTATION.jpg", "GRAPEDELIGHT.jpg", "ORANGEGATEAUX.JPG", "ALMONDMOCHA.JPG", "CHOCOVANILLA.JPG", "HOTANDSPICYCHOCOLATECAKE.JPG", "OREOMOCHA.JPG", "BLACKFOREST.JPG", "CHOCOWALNUT.JPG", "IRISHCOFFEE.JPG", "PINEAPPLE.JPG", "BLUEBERRYGATEAUX.JPG", "CHOCOCOLATE.JPG", "IRISHCOFFEEr.jpg", "PISTACIOGATEAUX.JPG", "BUTTERSCOTCH.JPG", "CHOCOLATCHIFFONGATEAUX.JPG", "KIWIGATEAUX.JPG", "RASPBERRY.JPG", "CHERRYNCHERRY.JPG", "CHOCOLATEFANTASY.JPG", "LITCHEEGATEAUX.JPG", "RICHSTRAWBERRY.JPG", "CHOCOBANANA.JPG", "CHOCOLATETRUFFLE.JPG", "MANGOFANTASY.JPG", "ROYALBLACKFOREST.JPG", "CHOCOFERRERROCHER.JPG", "DEATHBYCHOCOLATE.JPG", "MANGOFANTASYr.jpg", "VANILLA.JPG", "CHOCOMOCHA.JPG", "FIGANDHONEY.JPG", "MIXEDFRUIT1.JPG", "WHITEFOREST.JPG", "CHOCORASPBERRY.JPG", "FORESTINTHENEST.JPG", "MIXEDFRUIT.JPG", "WHITEFORESTr.jpg", "CHOCOSTRAWBERRY.JPG", "GERMANFOREST.JPG", "NUTBUTTERSCOTCH.JPG"],
+    "signature_cakes": ["3_d_cake_1.jpg", "3_d_cake2.jpg", "3_d_cake2new.jpg", "angle_cake.jpg", "black_and_white_forest.jpg", "blue_forest.jpg", "cashew_caramel.jpg", "choco_kit_kat.jpg", "chocolate_fruita_gateaux.jpg", "chocolate_temptation.jpg", "choco_nuts.jpg", "choco_temptation.jpg", "dark_forest.jpg", "dark_forestnew.jpg", "kings_crown.jpg", "kiwi_mousse_cake.jpg", "laddu_gateaux.jpg", "mango_delight.jpg", "mango(seasonal).jpg", "mocha_praline.jpg", "nougat_yogart_cheese_cake.jpg", "rainbow.jpg", "red_velvet_cake.jpg", "rich_chocolate.jpg", "rich_strwberry.jpg", "snow_forest.jpg", "strip_gateaux_copy.jpg", "strwberry_sweet_heart.jpg", "truffle_heart.jpg", "twin_chocolate.jpg", "twin_chocolatenw.jpg", "white_chocolate_truffle.jpg", "yule_log.jpg"],
+    "shaped_cakes": ["cs01(3.5kg).jpg", "cs02(5kg).jpg", "cs03(3kg).jpg", "cs04(5kg).jpg", "cs05(7kg).jpg", "cs06(5kg).jpg", "cs07(5kg).jpg", "cs08(4kg).jpg", "cs09(5kg).jpg", "cs10(4kg).jpg", "cs11(5kg).jpg", "cs12(4.5kg).jpg", "cs13(5kg).jpg", "cs14(3kg).jpg", "cs15(3kg).jpg", "cs16(7kg).jpg", "cs17(3kg).jpg", "cs18(3.5kg).jpg", "cs19(4.5kg)_copy.jpg", "cs19(4.5kg).jpg", "cs20(3.5kg).jpg", "cs21(3kg).jpg", "cs22(3.5kg).jpg", "cs23(3kg).jpg", "cs24(4kg).jpg", "cs25(5kg).jpg", "cs26(3kg).jpg", "cs27(5kg).jpg", "cs28(4kg).jpg", "cs29(3kg).jpg", "cs30(4kg).jpg", "cs32(5kg).jpg", "cs33(3.5kg).jpg", "cs34_(5kg).jpg", "cs35(4kg).jpg", "cs36(3.5kg).jpg", "cs37(4kg).jpg", "cs38(5kg).jpg", "cs39(5kg).jpg", "cs40(5kg).jpg", "cs41(5kg).jpg", "cs42(5kg).jpg", "cs43(3kg).jpg", "cs44(4kg).jpg", "cs45(5kg).jpg", "cs46(3.5kg).jpg", "cs47(3.5kg).jpg", "cs48(5kg).jpg", "cs49(5kg).jpg"],
+    "barbie_doll_cakes": ["br001_3.5kg.jpg", "br002_3.5kg.jpg", "br003_3.5kg.jpg", "br004_3.5kg.jpg", "br006_3.5kg.jpg", "br007_3.5kg.jpg", "br009_3.5_kg.jpg", "br010_3.5kg.jpg", "br012_3.5kg.jpg", "br013_3.5kg.jpg", "br014_6_kg.jpg", "br015_6kg.jpg", "br016_3.5kg.jpg", "br017_3.5kg.jpg", "br019_3.5kg.jpg", "br020_3.5kg.jpg", "br021_6kg.jpg", "br022_3.5kg.jpg", "br023_3.5kg.jpg", "br024_3.5kg.jpg", "br025_3.5kg.jpg", "br026_3.5kg.jpg", "br027_10_kg.jpg", "br028_3.5kg.jpg", "br029_3.5kg.jpg", "br030_6kg.jpg", "br031_3.5kg.jpg", "br033_6kg.jpg", "br034_6kg.jpg", "br036_3.5kg.jpg"],
+    "rice_paper_cakes": ["pr072.jpg", "r010.jpg", "rp001.jpg", "rp002.jpg", "rp003.jpg", "rp004.jpg", "rp005.jpg", "rp006.jpg", "rp007.jpg", "rp008.jpg", "rp009.jpg", "rp0100.jpg", "rp011.jpg", "rp012.jpg", "rp013.jpg", "rp014.jpg", "rp015.jpg", "rp016.jpg", "rp017.jpg", "rp018.jpg", "rp019.jpg", "rp020.jpg", "rp021.jpg", "rp022.jpg", "rp023.jpg", "rp024.jpg", "rp025.jpg", "rp026.jpg", "rp027.jpg", "rp028.jpg", "rp029.jpg", "rp030.jpg", "rp031.jpg", "rp032.jpg", "rp033.jpg", "rp034.jpg", "rp035.jpg", "rp036.jpg", "rp037.jpg", "rp038.jpg", "rp039.jpg", "rp040.jpg", "rp041.jpg", "rp042.jpg", "rp043.jpg", "rp044_(2).jpg", "rp044.jpg", "rp045.jpg", "rp046.jpg", "rp047.jpg", "rp048.jpg", "rp049.jpg", "rp050.jpg", "rp051.jpg", "rp052.jpg", "rp053.jpg", "rp054.jpg", "rp055.jpg", "rp056.jpg", "rp057.jpg", "rp058.jpg", "rp059.jpg", "rp060.jpg", "rp061.jpg", "rp062.jpg", "rp063.jpg", "rp064.jpg", "rp065.jpg", "rp066.jpg", "rp067.jpg", "rp068.jpg", "rp069.jpg", "rp070.jpg", "rp071.jpg", "rp073.jpg", "rp074.jpg", "rp075.jpg", "rp076.jpg", "rp077.jpg", "rp078.jpg", "rp079.jpg", "rp080.jpg", "rp081.jpg", "rp082.jpg", "rp083.jpg", "rp084.jpg", "rp085.jpg", "rp086.jpg", "rp087.jpg", "rp088.jpg", "rp089.jpg", "rp090.jpg", "rp091.jpg", "rp101.jpg", "rp102.jpg", "rp103.jpg", "rp104.jpg", "rp105.jpg", "rp106.jpg", "rp107.jpg", "rp108.jpg", "rp109.jpg", "rp110.jpg", "rp111.jpg", "rp112.jpg", "rp113.jpg", "rp114.jpg", "rp115.jpg", "rp116.jpg", "rp117.jpg", "rp118.jpg", "rp119.jpg", "rp120.jpg", "rp121.jpg", "rp122.jpg", "rp123.jpg", "rp124.jpg", "rp125.jpg", "rp126.jpg", "rp127.jpg", "rp128.jpg", "rp129.jpg", "rp130.jpg", "rp131.jpg", "rp132.jpg", "rp133.jpg", "rp134.jpg", "rp135.jpg", "rp136.jpg", "rp137.jpg", "rp138.jpg", "rp139.jpg", "rp140.jpg", "rp141.jpg", "rp142.jpg", "rp143.jpg", "rp144.jpg", "rp145.jpg", "rp146.jpg", "rp147.jpg", "rp148.jpg", "rp149.jpg", "rp150.jpg", "rp151.jpg", "rp152.jpg", "rp153.jpg", "rp154.jpg", "rp155.jpg", "rp156.jpg", "rp157.jpg", "rp158.jpg", "rp159.jpg", "rp160.jpg", "rp161.jpg", "rp162.jpg", "rp163.jpg", "rp164.jpg", "rp165.jpg", "rp166.jpg"],
+    "tiered_cakes": ["TC01(4KG).jpg", "TC07(4.5KG).jpg", "TC13(4.5KG).jpg", "TC21(12KG).jpg", "TC26.jpg", "TC33.jpg", "TC39.jpg", "TC45.jpg", "TC51.jpg", "TC57.jpg", "TC02(6KG).jpg", "TC08(4.5KG).jpg", "TC14(6KG).jpg", "TC22(6KG).jpg", "TC27.jpg", "TC34.jpg", "TC40.jpg", "TC46.jpg", "TC52.jpg", "TC58.jpg", "TC03(4.5KG).jpg", "TC09(6KG).JPG", "TC15(5KG).jpg", "TC23(8KG).jpg", "TC28.jpg", "TC35.jpg", "TC41.jpg", "TC47.jpg", "TC53.jpg", "TC59.jpg", "TC04(6KG).jpg", "TC10(6KG).jpg", "TC16(5KG).jpg", "TC23.jpg", "TC29.jpg", "TC36.jpg", "TC42.jpg", "TC48.jpg", "TC54.jpg", "TC60.jpg", "TC05(4.5KG).jpg", "TC11(6KG).jpg", "TC17(6KG).jpg", "TC24.jpg", "TC30.jpg", "TC37.jpg", "TC43.jpg", "TC49.jpg", "TC55.jpg", "TC06(6KG).jpg", "TC12(6KG).jpg", "TC18(4.5KG).jpg", "TC25.jpg", "TC31.jpg", "TC38.jpg", "TC44.jpg", "TC50.jpg", "TC56.jpg"],
+    "modelled_cakes": ["GP01(5kg).jpg", "GP11(8kg).jpg", "GP16(3.5kg).jpg", "GP36(3.jpg", "GP58(6kg).jpg", "GP75(4kg).jpg", "GP89(5.jpg", "GP02(6kg).jpg", "GP110(7kg).jpg", "GP17(4kg).jpg", "GP44(3kg).jpg", "GP59(4kg).jpg", "GP76(3.5kg).jpg", "GP90(3.5kg).jpg", "GP03(7kg).jpg", "GP111(6kg).jpg", "GP18(3.5kg).jpg", "GP45(3.5kg).jpg", "GP60(7kg).jpg", "GP76(3.jpg", "GP90(3.jpg", "GP04(5kg).jpg", "GP112(4.5kg).jpg", "GP19(4kg).JPG", "GP46(6kg).jpg", "GP61(5kg).jpg", "GP77(6kg).jpg", "GP91(6kg).jpg", "GP05(5kg).jpg", "GP112(4.jpg", "GP20(4kg).jpg", "GP47(7kg).jpg", "GP62(12kg).jpg", "GP78(5.5kg).jpg", "GP92(5.5kg).jpg", "GP06(4kg).jpg", "GP113(5.5kg).jpg", "GP21(5kg).JPG", "GP48(5kg).jpg", "GP63(4.5kg).jpg", "GP78(5.jpg", "GP92(5.jpg", "GP07(6kg).jpg", "GP113(5.jpg", "GP22(6kg).JPG", "GP48(7kg).jpg", "GP63(4.jpg", "GP79(3.5kg).jpg", "GP93(3.5kg).jpg", "GP08(7kg).jpg", "GP114(5kg).jpg", "GP23(4kg).jpg", "GP49(7kg).jpg", "GP64(9kg).jpg", "GP79(3.jpg", "GP93(3.jpg", "GP09(4.5kg).jpg", "GP114(5kg).jpg", "GP24(4kg).jpg", "GP50(3.5kg).jpg", "GP65(3.5kg).jpg", "GP80(3kg).jpg", "GP94(5.5kg).jpg", "GP10(4kg).jpg", "GP115(4kg).jpg", "GP25(3.5kg).jpg", "GP50(3.jpg", "GP65(3.jpg", "GP81(4kg).jpg", "GP94(5.jpg", "GP100(6kg).jpg", "GP116(6kg).jpg", "GP26(6kg).jpg", "GP51(3.5kg).jpg", "GP66(5.5kg).jpg", "GP82(5.5kg).jpg", "GP95(6kg).jpg", "GP101(5kg).jpg", "GP117(5kg).jpg", "GP27(4kg).jpg", "GP51(3.jpg", "GP66(5.jpg", "GP82(5.jpg", "GP96(3.5kg).jpg", "GP102(6kg).jpg", "GP118(5kg).jpg", "GP28(3.5kg).jpg", "GP52(3.5kg).jpg", "GP69(4kg).jpg", "GP83(3.5kg).jpg", "GP96(3.jpg", "GP103(3kg).jpg", "GP119(3.5kg).jpg", "GP28(3.jpg", "GP52(3.jpg", "GP70(8kg).jpg", "GP83(3.jpg", "GP97(4kg).jpg", "GP104(3.5kg).jpg", "GP119(3.jpg", "GP29(5kg).jpg", "GP53(3.5kg).jpg", "GP71(3.5kg).jpg", "GP84(6kg).jpg", "GP98(5kg).jpg", "GP104(3.jpg", "GP12(6kg).jpg", "GP30(6kg).jpg", "GP53(3.jpg", "GP71(3.jpg", "GP85(3.5kg).jpg", "GP99(5kg).jpg", "GP105(5kg).jpg", "GP120(3kg).jpg", "GP31(4kg).jpg", "GP54(3.5kg).jpg", "GP72(3.5kg).jpg", "GP85(3.jpg", "GP106(4kg).jpg", "GP121(4.5KG).jpg", "GP32(9kg).jpg", "GP54(3.jpg", "GP72(3.jpg", "GP86(6kg).jpg", "GP107(5kg).jpg", "GP121(4.jpg", "GP33(6kg).jpg", "GP55(6kg).jpg", "GP73(3.5kg).jpg", "GP87(3.5kg).jpg", "GP108(7kg).jpg", "GP13(4kg).JPG", "GP34(6kg).jpg", "GP56(4kg).jpg", "GP73(3.jpg", "GP87(3.jpg", "GP109(4.5kg).jpg", "GP14(3kg).jpg", "GP35(6kg).jpg", "GP57(5.5kg).jpg", "GP74(4.5kg).jpg", "GP88(4kg).jpg", "GP109(4.jpg", "GP15(6kg).jpg", "GP36(3.5kg).jpg", "GP57(5.jpg", "GP74(4.jpg", "GP89(5.g).jpg"]
+}
+
+// Disable back button as this can be a huge problem and no time to solve this atm.
+function disableBack() { window.history.forward() }
+window.onload = disableBack();
+window.onpageshow = function(evt) { if (evt.persisted) disableBack() }
+
+
+// Helper function to replace url params.
+function updateURLParameter(url, param, paramVal) {
+    var newAdditionalURL = "";
+    var tempArray = url.split("?");
+    var baseURL = tempArray[0];
+    var additionalURL = tempArray[1];
+    var temp = "";
+    if (additionalURL) {
+        tempArray = additionalURL.split("&");
+        for (var i = 0; i < tempArray.length; i++) {
+            if (tempArray[i].split('=')[0] != param) {
+                newAdditionalURL += temp + tempArray[i];
+                temp = "&";
+            }
+        }
+    }
+
+    var rows_txt = temp + "" + param + "=" + paramVal;
+    return baseURL + "?" + newAdditionalURL + rows_txt;
+}
+
+// Function to show cakes on the page.
+function showCakes(cake_type, index) {
+    index = typeof index !== 'undefined' ? index : '1';
+    // If index is undefined then the user has just entered the category. 
+    // So just show him the first three cakes.
+    var data;
+    var arrayLastIndex;
+    var html = "";
+    var contents = "";
+    if (index.p === undefined) {
+        data = objects_json[cake_type].slice(0, 3);
+        arrayLastIndex = objects_json[cake_type].indexOf(data.slice(-1)[0]);
+        var big_image = "";
+        var small_image = "";
+        $.each(data, function(index, cake) {
+            var path = "assets/images/" + cake_type + "/" + cake;
+            var name = path.split("/").slice(-1)[0].split(".j")[0];
+            var id = name.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-');
+            if (index == 0) {
+                // Then load the big image component
+                var source = $("#large-image").html();
+                var template = Handlebars.compile(source);
+                var context = { url: path, name: name, id: id };
+                big_image += template(context);
+            } else {
+                // Load the small image components
+                var source = $("#small-image").html();
+                var template = Handlebars.compile(source);
+                var context = { url: path, name: name, id: id };
+                small_image += template(context);
+            }
+        });
+        var source = $("#categories-first-page").html();
+        var template = Handlebars.compile(source);
+        var context = { big_image: big_image, small_image: small_image, category_name: cake_type.split("_").join(" ") };
+        html = template(context);
+    }
+    // The user is not on the first page. So now paginate and show 6 cakes to the user
+    // TODO also check if this is last page for the cakes and then if clicks next go to next category of cakes.
+    else {
+        var lastIndex = index.p;
+        // if lastIndex is 3 then we have shown him the first 3 already and now have to show him the next 6
+        // if lastIndex is 9 then we have shown him 9 (3 + 6) so we have to show him from the 9th element the next 6
+        var nextIndex = parseInt(lastIndex) + 6;
+        data = objects_json[cake_type].slice(lastIndex, nextIndex);
+        arrayLastIndex = objects_json[cake_type].indexOf(data.slice(-1)[0]);
+        $.each(data, function(index, cake) {
+            var path = "assets/images/" + cake_type + "/" + cake;
+            var name = path.split("/").slice(-1)[0].split(".j")[0];
+            var id = name.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-');
+            var source = $("#categories-second-page").html();
+            var template = Handlebars.compile(source);
+            var context = { url: path, name: name, category_name: cake_type.split("_").join(" "), id: id };
+            contents += template(context);
+        });
+        var source = $("#base-header-page").html();
+        var template = Handlebars.compile(source);
+        var context = { rows: contents };
+        var header = template(context);
+        html += header;
+    }
+    html += buildNavigation(arrayLastIndex);
+    $("#main-content").html(html);
+}
+
+// Function that builds the navigation of the page.
+function buildNavigation(lastIndex) {
+    lastIndex = lastIndex + 1; // Because array starts with index of 0 :)
+    var source = $("#navigation").html();
+    var template = Handlebars.compile(source);
+    var context = { lastIndex: lastIndex };
+    var html = template(context);
+    return html
+}
+
+// Helper function to get url params
+function getPageParams() {
+    params = window.location.href.split("?")[1];
+    if (params === undefined) {
+        return undefined;
+    } else {
+        [k, v] = params.split("=")
+        obj = new Object();
+        obj[k] = v
+        return obj;
+    }
+}
+
+
+function checkIfLastImageReacedInCategory(category, lastIndex) {
+    var length = objects_json[category].length;
+    if (lastIndex >= length) {
+        if (lastIndex - length == 6) {
+            return true;
+        } else { return false };
+    }
+}
+
+// Doc ready function, using this for routing and handing of clicks
+$(document).ready(function() {
+    if (window.location.hash === "") {
+        var source = document.getElementById("entry-page").innerHTML;
+        var template = Handlebars.compile(source);
+        var context = { title: "My New Post", body: "This is my first post!" };
+        var html = template(context);
+        $("#main-content").html(html);
+    } else if (window.location.hash.indexOf("#regular_cakes") != -1) {
+        var params = getPageParams();
+        showCakes("regular_cakes", params);
+    } else if (window.location.hash.indexOf("#signature_cakes") != -1) {
+        var params = getPageParams();
+        showCakes("signature_cakes", params);
+    } else if (window.location.hash.indexOf("#shaped_cakes") != -1) {
+        var params = getPageParams();
+        showCakes("shaped_cakes", params);
+    } else if (window.location.hash.indexOf("#barbie_doll_cakes") != -1) {
+        var params = getPageParams();
+        showCakes("barbie_doll_cakes", params);
+    } else if (window.location.hash.indexOf("#rice_paper_cakes") != -1) {
+        var params = getPageParams();
+        showCakes("rice_paper_cakes", params);
+    } else if (window.location.hash.indexOf("#tiered_cakes") != -1) {
+        var params = getPageParams();
+        showCakes("tiered_cakes", params);
+    } else if (window.location.hash.indexOf("#modelled_cakes") != -1) {
+        var params = getPageParams();
+        showCakes("modelled_cakes", params);
+    }
+
+    // Handling induvidual category clicks.
+    $("body").on("click", ".cake-btn", function(e) {
+        var category = $(e.target).attr("id");
+        window.location.hash = "#" + category;
+        showCakes(category);
+    });
+
+    $("body").on("click", "#home", function() {
+        window.location.hash = "";
+        window.location.reload();
+    });
+
+    $("body").on("click", "#next", function(e) {
+        var lastPagination = $(this).attr("data-pagination");
+        var hash = window.location.hash;
+        var category = hash.split("#")[1].split("?")[0];
+        window.history.replaceState('', '', updateURLParameter(window.location.href, "p", lastPagination));
+        e.preventDefault();
+        var params = getPageParams();
+        var lastIndex = parseInt(lastPagination) + 6;
+        if (checkIfLastImageReacedInCategory(category, lastIndex) == true) {
+            var nextCategory = oFunctions.keys.next(objects_json, category);
+            window.location.hash = "#" + nextCategory;
+            showCakes(nextCategory);
+        } else {
+            showCakes(category, params);
+        }
+    });
+
+    $("body").on("click", "#previous", function(e) {
+        var hash = window.location.hash;
+        var category = hash.split("#")[1].split("?")[0];
+        e.preventDefault();
+        var params = getPageParams();
+        var lastIndex = parseInt(params.p) - 6;
+        if (lastIndex < 0) {
+            window.location.hash = "#" + category;
+            showCakes(category);
+        } else {
+            window.history.replaceState('', '', updateURLParameter(window.location.href, "p", lastIndex));
+            showCakes(category, { p: lastIndex });
+        }
+    });
+
+    $("body").on("click", ".cake-image", function() {
+        var html = $("#modal-image").html();
+        var name = $(this).attr("src").split("/").slice(-1)[0].split(".j")[0];
+        var id = name.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-');
+        var template = Handlebars.compile(html);
+        var context = { image: $(this).attr("src"), name: name, id: id };
+        var html = template(context);
+        $("#main-content").append($(html));
+        $("#" + id).modal('show');
+    });
+
+    $("body").on("click", ".close-btn", function(e) {
+        e.preventDefault();
+        return false;
+        // var html = $("#modal-image").remove();
+        // $("#main-content").removeClass("hidden");
+    });
+});
+
+
+//Object Helper Functions
+oFunctions = {};
+oFunctions.keys = {};
+
+//NEXT KEY
+oFunctions.keys.next = function(o, id) {
+    var keys = Object.keys(o),
+        idIndex = keys.indexOf(id),
+        nextIndex = idIndex += 1;
+    if (nextIndex >= keys.length) {
+        //we're at the end, there is no next
+        return;
+    }
+    var nextKey = keys[nextIndex]
+    return nextKey;
+};
+
+//PREVIOUS KEY
+oFunctions.keys.previous = function(o, id) {
+    var keys = Object.keys(o),
+        idIndex = keys.indexOf(id),
+        nextIndex = idIndex -= 1;
+    if (idIndex === 0) {
+        //we're at the beginning, there is no previous
+        return;
+    }
+    var nextKey = keys[nextIndex]
+    return nextKey;
+};
